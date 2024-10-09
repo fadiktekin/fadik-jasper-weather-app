593c5d2746a691992c9934ade90a5d521b8ba188
Fadik Tekin <tekinfadik@gmail.com>
2024-10-09 11:39:39 +0200

    create activity form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,3 +1,30 @@
-export function Form() {
-  return <div>FORM</div>;
+import "./Form.css";
+export function Form({ handleAddActivity }) {
+  function onSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData);
+
+    const result = {
+      name: data.name,
+      isForGoodWeather: data.isForGoodWeather === "on",
+    };
+
+    console.log(result);
+  }
+
+  return (
+    <form aria-labelledby="title" className="activity-form" onSubmit={onSubmit}>
+      <h2 id="title">Add new Activity</h2>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+      </div>
+      <div>
+        <label htmlFor="good-weather">Good-weather activity</label>
+        <input type="checkbox" name="isForGoodWeather" id="good-weather" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
